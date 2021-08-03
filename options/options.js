6ee839b1225ec6e501c1fbd7229cd3a3f6078fea
Crowdbotics <team@crowdbotics.com>
2021-08-03 20:48:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tiny_brook_29426Identifier",
+  url: "https://tiny_brook_29426Identifier.botics.co",
+  api: "https://tiny_brook_29426Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
